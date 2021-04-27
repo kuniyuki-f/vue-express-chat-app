e199fd6908067bc42122551b74ac561dfd0a7828
c501306014 <c501306014@gmail.com>
2021-04-27 13:26:03 +0900

    update axios, vue-axiosを追加

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import store from "./store";
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import router from './router';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VueAxios, axios);
 
 new Vue({
   vuetify,
