8057ed136febb58ed3607595a27195e36cc58c09
c501306014 <c501306014@gmail.com>
2021-05-17 14:11:18 +0900

    コメントアウトしていた箇所などを削除

--- public/js/socket_manager.js
+++ public/js/socket_manager.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser')
 const passportSocketio = require('passport.socketio');
 
 exports.socket_manager = class {
-    constructor(server, sessionMiddleware, passport, sessionStore) {
+    constructor(server, sessionMiddleware,) {
         this.io = require('socket.io')(server, {
             cors: {
                 origin: true,
@@ -15,63 +15,13 @@ exports.socket_manager = class {
             }
         });
 
-
-        // this.sessionMiddleware = sessionMiddleware;
-        // this.passport = passport;
-        // this.sessionStore = sessionStore;
-
         const wrap = middleware => (socket, next) => middleware(socket.request, {}, next);
-
-        // this.io.use(wrap(this.sessionMiddleware));
-
-        // this.io.use(wrap(this.passport.initialize()));
-        // this.io.use(wrap(this.passport.session()));
-        // this.io.use(passportSocketio.authorize({
-        //     passport: this.passport,
-        //     cookieParser: cookieParser,
-        //     key: 'express.sid',
-        //     secret: 'keyboard cat',
-        //     store: this.sessionStore,
-        //     success: this.onAuthorizeSuccess,
-        //     fail: this.onAuthorizeFail,
-        // }))
-
-        // this.io.use((socket, next) => {
-        //     if (socket.request.user) {
-        //         next();
-        //     } else {
-        //         next(new Error("unauthoraized"));
-        //     }
-        // })
-        // this.io.use(function (socket, next) {
-        //     passport(socket.request, socket.request.res, next);
-        // });
-    }
-    onAuthorizeSuccess = function (data, accept) {
-        console.log('successful connection to socket.io');
-        accept(null, true);
-
-        // OR
-
-        // If you use socket.io@1.X the callback looks different
-        accept();
-    }
-
-    onAuthorizeFail = function (data, message, error, accept) {
-        if (error)
-            throw new Error(message);
-        console.log('failed connection to socket.io:', message);
-        // console.log(data)
+        this.io.use(wrap(sessionMiddleware));
     }
     startSocket = function () {
         this.io.on('connection', async socket => {
-            console.log("ユーザが接続しました");
-            // console.log(socket.request.session.id)
-            console.log("これ！！！", socket.request.session)
-
-
-
-
+            console.log("socket.io: ユーザが接続しました");
+            console.log(socket.request.session)
 
             const sql = `SELECT * FROM message;`;
 
@@ -80,8 +30,6 @@ exports.socket_manager = class {
             if (result) {
                 this.io.emit('s2c_log', result['rows']);
             }
-
-
 
             socket.on('c2s_message', async data => {
                 data.message = xssFilters.inHTMLData(data.message);
